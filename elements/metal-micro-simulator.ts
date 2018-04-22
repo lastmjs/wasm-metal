7ae0e19b426ef31ef2e627bcc29874457746937d
Jordan Last <jordan.michael.last@gmail.com>
2018-04-21 21:00:22 -0600

    allow number of registers to change dynamically

--- elements/metal-micro-simulator.ts
+++ elements/metal-micro-simulator.ts
@@ -2,11 +2,12 @@ import {html, render} from 'lit-html/lib/lit-extended.js';
 import {createStore} from 'redux';
 
 interface State {
-
+    numberOfRegisters: number;
 }
 
 interface Action {
     type: string;
+    numberOfRegisters: number;
 }
 
 interface StateChangeEvent extends CustomEvent {
@@ -15,9 +16,17 @@ interface StateChangeEvent extends CustomEvent {
     }
 }
 
-const InitialState: State = {};
+const InitialState: State = {
+    numberOfRegisters: 10
+};
 const RootReducer = (state=InitialState, action: Action) => {
     switch (action.type) {
+        case 'CHANGE_NUMBER_OF_REGISTERS': {
+            return {
+                ...state,
+                numberOfRegisters: action.numberOfRegisters > 0 ? action.numberOfRegisters : 1
+            };
+        }
         default: {
             return state;
         }
@@ -30,13 +39,51 @@ class MetalMicroSimulator extends HTMLElement {
     constructor() {
         super();
         Store.subscribe(this.render.bind(this));
+        this.render();
+    }
+
+    numberOfRegistersChanged(e) {
+        Store.dispatch({
+            type: 'CHANGE_NUMBER_OF_REGISTERS',
+            numberOfRegisters: parseInt(e.target.value)
+        });
     }
 
     render() {
         const state = Store.getState();
 
         render(html`
-            Micro simulator
+            <style>
+                .hexCodeInput {
+                    display: inline-block;
+                    border: solid 1px black;
+                    min-width: 25vw;
+                }
+            </style>
+
+            <h1>WASM Metal</h1>
+            <h2>Microarchitecture Simulator</h2>
+
+            <div>
+                Hex code to execute: 0x<span id="hexCodeInput" contenteditable class="hexCodeInput"></span>
+            </div>
+
+            <div>
+                Number of registers: <input type="number" value="10" oninput="${(e) => this.numberOfRegistersChanged(e)}">
+            </div>
+
+            <br>
+
+            <div>
+                Registers
+                ${new Array(state.numberOfRegisters).fill(0).map((x, index) => {
+                    return html`
+                        <div>
+                            ${index}: <input id="register${index}" type="text">
+                        </div>
+                    `;
+                })}
+            </div>
         `, this);
     }
 }
